ba4a2786168744fffcab7d36d94316c1c1ec65f6
Samuel Adelowo <samueladelowo92@gmail.com>
2023-08-28 13:36:00 +0100

    sign in dialog

--- lib/screens/onboarding/onboarding_screen.dart
+++ lib/screens/onboarding/onboarding_screen.dart
@@ -2,8 +2,10 @@ import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:rive/rive.dart';
 
+import '../../components/components.dart';
 import '../../widgets/widgets.dart';
 
 class OnboardingScreen extends StatefulWidget {
@@ -19,7 +21,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   @override
   void initState() {
     super.initState();
-    _buttonAnimationController = OneShotAnimation('active',autoplay: false);
+    _buttonAnimationController = OneShotAnimation('active', autoplay: false);
   }
 
   @override
@@ -27,44 +29,84 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     return Scaffold(
       body: Stack(
         children: [
-          const RiveAnimation.asset('assets/RiveAssets/shapes.riv',
-          fit: BoxFit.cover,),
-          Positioned.fill(child: BackdropFilter(filter: ImageFilter.blur(sigmaX: 20, sigmaY: 10),
-            )),
+          const RiveAnimation.asset(
+            'assets/RiveAssets/shapes.riv',
+            fit: BoxFit.cover,
+          ),
+          Positioned.fill(
+              child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 20, sigmaY: 10),
+          )),
           Positioned(
-            width: MediaQuery.of(context).size.width*1.7,
+              width: MediaQuery.of(context).size.width * 1.7,
               bottom: 200,
               left: 100,
               child: Image.asset('assets/Backgrounds/Spline.png')),
-
-          Positioned.fill(child: BackdropFilter(filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
-            child: const SizedBox(),)),
+          Positioned.fill(
+              child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
+            child: const SizedBox(),
+          )),
           SafeArea(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 25.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Spacer(),
+                  const Spacer(
+                    flex: 2,
+                  ),
                   SizedBox(
                     width: 260,
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [Text('Learn design & code', style: TextStyle(
-                        fontSize: 60,
-                        fontFamily: 'Poppins',
-                        height: 1.2,
-                      ),),
-                      SizedBox(height: 15,), 
-                      Text('Don’t skip design. Learn design and code, by building real apps with Flutter and Swift. Complete courses about the best tools.'),
+                      children: const [
+                        Text(
+                          'Learn design & code',
+                          style: TextStyle(
+                            fontSize: 60,
+                            fontFamily: 'Poppins',
+                            height: 1.2,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                            'Don’t skip design. Learn design and code, by building real apps with Flutter. Complete courses about the best tools.'),
                       ],
                     ),
                   ),
-                  Spacer(),
-                  AnimatedButton(buttonAnimationController: _buttonAnimationController, press: (){_buttonAnimationController.isActive = true;},),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 24.0),
-                    child: Text('Purchase includes access to 30+ courses, 240+ premium tutorials, 120+ hours of videos, source files and certificates.'),
+                  const Spacer(flex: 2),
+                  AnimatedButton(
+                    buttonAnimationController: _buttonAnimationController,
+                    press: () {
+                      _buttonAnimationController.isActive = true;
+                      Future.delayed(
+                        const Duration(milliseconds: 800),
+                            () {
+                          setState(() {
+                            isSignInDialogShown = true;
+                          });
+                          // Let's add the slide animation while dialog shows
+                          customSignInDialog(
+                            context,
+                            onClosed: (_) {
+                              setState(() {
+                                isSignInDialogShown = false;
+                              });
+                            },
+                          );
+                        },
+                      );
+
+                    },
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 24.0),
+                    child: Text(
+                        'Purchase includes access to 30+ courses, 240+ premium tutorials, 120+ hours of videos, source files and certificates.',
+                    textAlign: TextAlign.center,),
                   )
                 ],
               ),
@@ -75,5 +117,4 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     );
   }
 }
-
 
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,1 +1,2 @@
 export 'animated_button.dart';
+export 'sign_in_form.dart';
